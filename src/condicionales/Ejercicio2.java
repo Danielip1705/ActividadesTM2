38c17cc89a0c7f209ecbf961719a4582fa619fab
Danielip1705 <daniel.iglesias@iesnervion.es>
2023-11-02 13:46:55 +0100

    Ejercicios Condicionales

--- src/condicionales/Ejercicio2.java
+++ src/condicionales/Ejercicio2.java
@@ -7,30 +7,7 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 		//Indicamos con la variable int el dni
 		int dni;
-		int resto;
-		String T="0";
-		String R="1";
-		String W="2";
-		String A="3";
-		String G="4";
-		String M="5";
-		String Y="6";
-		String F="7";
-		String P="8";
-		String D="9";
-		String X="10";
-		String B="11";
-		String N="12";
-		String J="13";
-		String Z="14";
-		String S="15";
-		String Q="16";
-		String V="17";
-		String H="18";
-		String L="19";
-		String C="20";
-		String K="21";
-		String E="22";
+		int result=0;
 		
 		//creamos scanner
 		Scanner sc = new Scanner(System.in);
@@ -41,8 +18,17 @@ public class Ejercicio2 {
 		//escribe dni
 		dni = sc.nextInt();
 		
+		dni = result;
+		
 		//hacemos la operacion para 
 		dni%=23;
+		switch (dni) {
+		case 0:
+			System.out.println(result+"T");
+		case 1:
+			System.out.println(result+"R");
+			
+		}
 		
 		
 		
