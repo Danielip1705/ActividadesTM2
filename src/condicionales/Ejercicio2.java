52a5353c4f6ad9a774f4e86d02c30cce4534433d
Danielip1705 <daniel.iglesias@iesnervion.es>
2023-11-10 14:38:09 +0100

    Ejercicios condicionales

--- src/condicionales/Ejercicio2.java
+++ src/condicionales/Ejercicio2.java
@@ -2,112 +2,105 @@ package condicionales;
 
 import java.util.Scanner;
 
+/*/ ENTRADA:31884794 | RES.ESPERADO:31884794D | RES.OBTENIDO:31884794D 
+ * ENTRADA:31884793 | RES.ESPERADO:31884793P | RES.OBTENIDO:31884793P 
+ */
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		//Indicamos con la variable int el dni
+		// Indicamos con la variable int el dni
 		int dni;
-		int result=0;
-		
-		//creamos scanner
+		int result = 0;
+
+		// creamos scanner
 		Scanner sc = new Scanner(System.in);
-		
-		//pedimos el dni
+
+		// pedimos el dni
 		System.out.println("Inserta su dni");
-		
-		//escribe dni
+
+		// escribe dni
 		dni = sc.nextInt();
-		
-		//El dni se almacena en result
-		result =dni;
-		
-		//hacemos la operacion para saber la letra
-		dni%=23;
-		
-		//declaramos 23 casos, cada caso es la letra correspondiente al dni
+
+		// El dni se almacena en result
+		result = dni;
+
+		// hacemos la operacion para saber la letra
+		dni %= 23;
+
+		// declaramos 23 casos, cada caso es la letra correspondiente al dni
 		switch (dni) {
 		case 0:
-			System.out.println(result+"T");
+			System.out.println(result + "T");
 			break;
 		case 1:
-			System.out.println(result+"R");
+			System.out.println(result + "R");
 			break;
 		case 2:
-			System.out.println(result+"W");
+			System.out.println(result + "W");
 			break;
 		case 3:
-			System.out.println(result+"A");
+			System.out.println(result + "A");
 			break;
 		case 4:
-			System.out.println(result+"G");
+			System.out.println(result + "G");
 			break;
 		case 5:
-			System.out.println(result+"M");
+			System.out.println(result + "M");
 			break;
 		case 6:
-			System.out.println(result+"Y");
+			System.out.println(result + "Y");
 			break;
 		case 7:
-			System.out.println(result+"F");
+			System.out.println(result + "F");
 			break;
 		case 8:
-			System.out.println(result+"P");
+			System.out.println(result + "P");
 			break;
 		case 9:
-			System.out.println(result+"D");
+			System.out.println(result + "D");
 			break;
 		case 10:
-			System.out.println(result+"X");
+			System.out.println(result + "X");
 			break;
 		case 11:
-			System.out.println(result+"B");
+			System.out.println(result + "B");
 			break;
 		case 12:
-			System.out.println(result+"N");
+			System.out.println(result + "N");
 			break;
 		case 13:
-			System.out.println(result+"J");
+			System.out.println(result + "J");
 			break;
 		case 14:
-			System.out.println(result+"Z");
+			System.out.println(result + "Z");
 			break;
 		case 15:
-			System.out.println(result+"S");
+			System.out.println(result + "S");
 			break;
 		case 16:
-			System.out.println(result+"Q");
+			System.out.println(result + "Q");
 			break;
 		case 17:
-			System.out.println(result+"V");
+			System.out.println(result + "V");
 			break;
 		case 18:
-			System.out.println(result+"H");
+			System.out.println(result + "H");
 			break;
 		case 19:
-			System.out.println(result+"L");
+			System.out.println(result + "L");
 			break;
 		case 20:
-			System.out.println(result+"C");
+			System.out.println(result + "C");
 			break;
 		case 21:
-			System.out.println(result+"K");
+			System.out.println(result + "K");
 			break;
 		case 22:
-			System.out.println(result+"E");
-			
-	//cerramos scanner
-	sc.close();
+			System.out.println(result + "E");
+
+			// cerramos scanner
+			sc.close();
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
 
 }
--- src/condicionales/Ejercicio7.java
+++ src/condicionales/Ejercicio7.java
@@ -36,26 +36,30 @@ public class Ejercicio7 {
 		if (segundos == 60) {
 			segundos = 0;
 			minutos++;
-		} else if (minutos == 59) {
+		} if (minutos == 60) {
 			minutos = 0;
 			horas++;
-		} else if (horas == 24) {
+		} if (horas == 24) {
 			horas = 0;
 		}
 		// imprimimos en pantalla que segun el resultado que obtengamos, que nos ponga
 		// un 0(para que se vea mejor)
-		if (horas > 10 && minutos > 10 && segundos > 10) {
-			System.out.println(horas + ":" + minutos + ":" + segundos);
-		} else if (horas > 10 && minutos > 10) {
-			System.out.println(horas + ":" + minutos + ":0" + segundos);
-		} else if (horas > 10) {
-			System.out.println(horas + ":0" + minutos + ":0" + segundos);
-		} else if (horas < 10 && minutos < 10 && segundos < 10) {
+		if (horas < 10 && minutos < 10 && segundos < 10) {
 			System.out.println("0" + horas + ":0" + minutos + ":0" + segundos);
-		} else if (segundos > 10) {
+		} else if (horas < 10 && minutos < 10) {
 			System.out.println("0" + horas + ":0" + minutos + ":" + segundos);
-		} else if (minutos > 10) {
+		} else if (horas < 10 && segundos < 10) {
 			System.out.println("0" + horas + ":" + minutos + ":0" + segundos);
+		} else if (minutos < 10 && segundos < 10) {
+			System.out.println(horas + ":0" + minutos + ":0" + segundos);
+		} else if (horas < 10) {
+			System.out.println("0" + horas + ":" + minutos + ":" + segundos);
+		} else if (minutos < 10) {
+			System.out.println(horas + ":0" + minutos + ":" + segundos);
+		} else if (segundos < 10) {
+			System.out.println(horas + ":" + minutos + ":0" + segundos);
+		} else {
+			System.out.println(horas + ":" + minutos + ":" + segundos);
 		}
 
 		// cerramos scanner
